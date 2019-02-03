bfe797f7292e3f97e9795dbb48df1b7095ab21e3
TagruP <purgat.pawel@gmail.com>
2019-02-03 11:30:56 +0100

    Add DTO interface

--- ExpensePrediction.DataTransferObjects/Category/CategoryDto.cs
+++ ExpensePrediction.DataTransferObjects/Category/CategoryDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace ExpensePrediction.DataTransferObjects.Category
 {
-    public class CategoryDto
+    public class CategoryDto : IDataTransferObject
     {
         public string Id { get; set; }
         public string Name { get; set; }
