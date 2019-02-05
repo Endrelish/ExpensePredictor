0e8938ab4e474dbc39aee3145171a57e801ed581
Paweł Purgat <purgat.pawel@gmail.com>
2019-02-05 13:40:08 +0100

    Periodically compute model

--- ExpensePrediction.WebAPI/HostedRegressionService.cs
+++ ExpensePrediction.WebAPI/HostedRegressionService.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using ExpensePrediction.BusinessLogicLayer.Interfaces.Services;
+﻿using ExpensePrediction.BusinessLogicLayer.Interfaces.Services;
 using ExpensePrediction.DataAccessLayer.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ExpensePrediction.WebAPI
 {
     public class HostedRegressionService : IHostedService
     {
         private readonly IServiceScopeFactory _scopeFactory;
+        private readonly System.Timers.Timer _timer;
 
         public HostedRegressionService(IServiceScopeFactory scopeFactory)
         {
             _scopeFactory = scopeFactory;
+            _timer = new System.Timers.Timer(24 * 60 * 60 * 1000);
+            _timer.Elapsed += async (s, a) => await ComputeAsync();
+            _timer.AutoReset = true;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await ComputeAsync();
+            _timer.Start();
+        }
+
+        public async Task ComputeAsync()
         {
             using (var scope = _scopeFactory.CreateScope())
             {
@@ -39,6 +49,7 @@ namespace ExpensePrediction.WebAPI
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
+            _timer.Stop();
             return Task.CompletedTask;
         }
     }
